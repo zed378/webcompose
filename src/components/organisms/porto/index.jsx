ff56d23bc4eeb4ac13fdaaea1335530cecdce046
zed378 <kembedt@gmail.com>
2023-10-26 15:48:14 +0700

    add porto section

--- src/components/organisms/porto/index.jsx
+++ src/components/organisms/porto/index.jsx
@@ -1,8 +1,10 @@
 import { Fade } from "react-awesome-reveal";
+import { portoCard, portoMobile } from "@hooks/staticData";
+
+// components
+import PortoCardMobile from "@components/molecules/porto";
 
 // assets
-import mentari from "@assets/images/porto/mentari.png";
-import dayat from "@assets/images/porto/dayat.png";
 import { BsDot, BsArrowUpRight } from "react-icons/bs";
 
 export default function Porto() {
@@ -30,116 +32,118 @@ export default function Porto() {
       </Fade>
 
       <Fade direction="left">
-        <button className="px-5 py-3 text-white rounded-lg bg-[#644A98] hover:bg-[#6f35e4] tablet:flex items-center text-sm mt-5 phone:hidden ">
+        <a
+          href="https://github.com/zed378"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="px-5 py-3 text-white w-52 rounded-lg bg-[#644A98] hover:bg-[#6f35e4] tablet:flex items-center justify-center text-sm mt-5 phone:hidden "
+        >
           Check Full Portofolio <BsDot className="text-xl" />
-        </button>
+        </a>
       </Fade>
 
       <Fade direction="down" cascade duration={500}>
-        <ul className="w-full flex flex-wrap justify-between mt-20 ">
+        <ul className="w-full laptop:flex flex-wrap justify-between mt-20 phone:hidden ">
+          {/* Mentari edukids */}
           <li
-            className="w-[59.5%] bg-gray-300 rounded-t-3xl rounded-bl-3xl h-[35rem] relative overflow-hidden bg-cover bg-center "
-            style={{ backgroundImage: `url(${mentari})` }}
+            className="laptop:w-[59.5%] phone:w-full bg-gray-300 rounded-t-3xl rounded-bl-3xl ultra:h-[35rem] pc:h-[32rem] desktop:h-[30rem] laptop:h-[28rem] phone:h-[30rem] relative overflow-hidden bg-cover bg-top "
+            style={{ backgroundImage: `url(${portoCard[0].img})` }}
           >
             <div className="w-full h-full absolute top-0 left-0 grad -z-0 "></div>
 
-            <div className="w-[80%] flex flex-col gap-6 absolute bottom-0 left-0 pl-12 pr-20 pb-10 ">
+            <div className="w-[85%] flex flex-col gap-6 absolute bottom-0 left-0 pl-12 pr-20 pb-10 ">
               <Fade direction="up" duration={2000}>
-                <h1 className="text-white text-2xl ">Mentari Edukids</h1>
+                <h1 className="text-white text-2xl ">{portoCard[0].title}</h1>
               </Fade>
               <Fade direction="up" duration={2000}>
                 <p className="text-white text-sm font-extralight ">
-                  Mentari Edukids is a tutoring institution that uses the
-                  Montessori teaching method as its curriculum. This project is
-                  a simple landing page that explains the programs and packages
-                  offered by this tutoring place and at the end of the page
-                  there will be a call to action that leads to the WhatsApp
-                  application for further communication.
+                  {portoCard[0].desc}
                 </p>
               </Fade>
             </div>
 
-            <div className="bg-white flex items-center justify-center rounded-tl-[36px] absolute bottom-0 right-0 z-10 h-40 w-40 ">
+            <div className="bg-white flex items-center justify-center rounded-tl-[36px] absolute bottom-0 right-0 z-10 ultra:h-40 ultra:w-40 pc:h-32 pc:w-32 desktop:w-28 desktop:h-28 laptop:w-24 laptop:h-24 ">
               <a
-                href="https://mentariedukids.my.id"
+                href={portoCard[0].link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-[#5A3E92] rounded-[36px] cursor-pointer hover:bg-violet-600 w-[7.5rem] h-[7.5rem] flex items-center justify-center "
+                className="bg-[#5A3E92] pc:rounded-[36px] desktop:rounded-[30px] laptop:rounded-[26px] cursor-pointer hover:bg-violet-600 ultra:w-[7.5rem] ultra:h-[7.5rem] pc:w-[6.5rem] pc:h-[6.5rem] desktop:w-[5.5rem] desktop:h-[5.5rem] laptop:w-[4.5rem] laptop:h-[4.5rem] flex items-center justify-center "
               >
                 <BsArrowUpRight className="text-white text-2xl font-black " />
               </a>
             </div>
           </li>
+          {/* End of Mentari edukids */}
 
+          {/* Wedding Invitation */}
           <li
-            className="w-[39.5%] bg-gray-300 rounded-t-3xl rounded-bl-3xl h-[35rem] relative overflow-hidden bg-cover bg-center "
-            style={{ backgroundImage: `url(${dayat})` }}
+            className="laptop:w-[39.5%] laptop:mt-0 phone:mt-4 phone:w-full bg-gray-300 rounded-t-3xl rounded-bl-3xl ultra:h-[35rem] pc:h-[32rem] desktop:h-[30rem] laptop:h-[28rem] phone:h-[30rem] relative overflow-hidden bg-cover bg-top "
+            style={{ backgroundImage: `url(${portoCard[1].img})` }}
           >
             <div className="w-full h-full absolute top-0 left-0 grad -z-0 "></div>
 
-            <div className="w-[80%] flex flex-col gap-6 absolute bottom-0 left-0 pl-12 pr-20 pb-10 ">
+            <div className="desktop:w-[85%] laptop:w-[95%] flex flex-col gap-6 absolute bottom-0 left-0 pl-12 pr-20 pb-10 ">
               <Fade direction="up" duration={2000}>
-                <h1 className="text-white text-2xl ">
-                  Digital Wedding Invitation
-                </h1>
+                <h1 className="text-white text-2xl ">{portoCard[1].title}</h1>
               </Fade>
               <Fade direction="up" duration={2000}>
                 <p className="text-white text-sm font-extralight ">
-                  A digitalized wedding invitation project to make it easier to
-                  send invitations and reduce paper. This project has parameters
-                  that can be filled in dynamically so that each invited guest
-                  has a private invitation.
+                  {portoCard[1].desc}
                 </p>
               </Fade>
             </div>
 
-            <div className="bg-white flex items-center justify-center rounded-tl-[36px] absolute bottom-0 right-0 z-10 h-40 w-40 ">
+            <div className="bg-white flex items-center justify-center rounded-tl-[36px] absolute bottom-0 right-0 z-10 ultra:h-40 ultra:w-40 pc:h-32 pc:w-32 desktop:w-28 desktop:h-28 laptop:w-24 laptop:h-24 ">
               <a
-                href="https://www.dayatlubena.my.id/?to=Zawawi+dan+Eva"
+                href={portoCard[1].link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-[#5A3E92] rounded-[36px] cursor-pointer hover:bg-violet-600 w-[7.5rem] h-[7.5rem] flex items-center justify-center "
+                className="bg-[#5A3E92] pc:rounded-[36px] desktop:rounded-[30px] laptop:rounded-[26px] cursor-pointer hover:bg-violet-600 ultra:w-[7.5rem] ultra:h-[7.5rem] pc:w-[6.5rem] pc:h-[6.5rem] desktop:w-[5.5rem] desktop:h-[5.5rem] laptop:w-[4.5rem] laptop:h-[4.5rem] flex items-center justify-center "
               >
                 <BsArrowUpRight className="text-white text-2xl font-black " />
               </a>
             </div>
           </li>
+          {/* End of Wedding Invitation */}
 
+          {/* Travel Diary */}
           <li
-            className="w-full mt-4 bg-gray-300 rounded-t-3xl rounded-bl-3xl h-[35rem] relative overflow-hidden bg-cover bg-center "
-            style={{ backgroundImage: `url(${mentari})` }}
+            className="w-full mt-4 bg-gray-300 rounded-t-3xl rounded-bl-3xl ultra:h-[35rem] pc:h-[32rem] desktop:h-[30rem] laptop:h-[28rem] phone:h-[30rem] relative overflow-hidden bg-cover bg-top "
+            style={{ backgroundImage: `url(${portoCard[2].img})` }}
           >
             <div className="w-full h-full absolute top-0 left-0 grad -z-0 "></div>
 
-            <div className="w-[50%] flex flex-col gap-6 absolute bottom-0 left-0 pl-12 pr-20 pb-10 ">
+            <div className="desktop:w-[50%] laptop:w-4/5 flex flex-col gap-6 absolute bottom-0 left-0 pl-12 pr-20 pb-10 ">
               <Fade direction="up" duration={2000}>
-                <h1 className="text-white text-2xl ">Mentari Edukids</h1>
+                <h1 className="text-white text-2xl ">{portoCard[2].title}</h1>
               </Fade>
               <Fade direction="up" duration={2000}>
                 <p className="text-white text-sm font-extralight ">
-                  Mentari Edukids is a tutoring institution that uses the
-                  Montessori teaching method as its curriculum. This project is
-                  a simple landing page that explains the programs and packages
-                  offered by this tutoring place and at the end of the page
-                  there will be a call to action that leads to the WhatsApp
-                  application for further communication.
+                  {portoCard[2].desc}
                 </p>
               </Fade>
             </div>
 
-            <div className="bg-white flex items-center justify-center rounded-tl-[36px] absolute bottom-0 right-0 z-10 h-40 w-40 ">
+            <div className="bg-white flex items-center justify-center rounded-tl-[36px] absolute bottom-0 right-0 z-10 ultra:h-40 ultra:w-40 pc:h-32 pc:w-32 desktop:w-28 desktop:h-28 laptop:w-24 laptop:h-24 ">
               <a
-                href="https://mentariedukids.my.id"
+                href={portoCard[2].link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-[#5A3E92] rounded-[36px] cursor-pointer hover:bg-violet-600 w-[7.5rem] h-[7.5rem] flex items-center justify-center "
+                className="bg-[#5A3E92] pc:rounded-[36px] desktop:rounded-[30px] laptop:rounded-[26px] cursor-pointer hover:bg-violet-600 ultra:w-[7.5rem] ultra:h-[7.5rem] pc:w-[6.5rem] pc:h-[6.5rem] desktop:w-[5.5rem] desktop:h-[5.5rem] laptop:w-[4.5rem] laptop:h-[4.5rem] flex items-center justify-center "
               >
                 <BsArrowUpRight className="text-white text-2xl font-black " />
               </a>
             </div>
           </li>
+          {/* End of Travel Diary */}
         </ul>
       </Fade>
+
+      <ul className="laptop:hidden w-full mt-20 flex flex-col gap-6">
+        {portoMobile.map((item, idx) => (
+          <PortoCardMobile item={item} key={idx} />
+        ))}
+      </ul>
     </div>
   );
 }
--- src/hooks/staticData.js
+++ src/hooks/staticData.js
@@ -1,7 +1,16 @@
+// about assets
 import date from "@assets/images/about/date.png";
 import mark from "@assets/images/about/mark.png";
 import money from "@assets/images/about/money.png";
 import search from "@assets/images/about/search.png";
+
+// porto assets
+import mentari from "@assets/images/porto/mentari.png";
+import mentarim from "@assets/images/porto/mentarim.png";
+import dayat from "@assets/images/porto/dayat.png";
+import dayatm from "@assets/images/porto/dayatm.png";
+import journey from "@assets/images/porto/journey.png";
+import journeym from "@assets/images/porto/journeym.png";
 
 export const cardData = [
   {
@@ -23,5 +32,47 @@ export const cardData = [
     title: "Satisfying",
     desc: "Team members all over the world",
     img: money,
+  },
+];
+
+export const portoCard = [
+  {
+    title: "Mentari Edukids",
+    desc: "Mentari Edukids is a tutoring institution that uses the Montessori teaching method as its curriculum. This project is a simple landing page that explains the programs and packages offered by this tutoring place and at the end of the page there will be a call to action that leads to the WhatsApp application for further communication.",
+    img: mentari,
+    link: "https://mentariedukids.my.id",
+  },
+  {
+    title: "Digital Wedding Invitation",
+    desc: " A digitalized wedding invitation project to make it easier to send invitations and reduce paper. This project has parameters that can be filled in dynamically so that each invited guest has a private invitation.",
+    img: dayat,
+    link: "https://www.dayatlubena.my.id/?to=Zawawi+dan+Eva",
+  },
+  {
+    title: "The Journey",
+    desc: "The Journey is a simple blog built using ReactJs and ExpressJS. This application has features for posting posts, commenting on posts and liking posts. This application is like a personal diary that we can share with other people.",
+    img: journey,
+    link: "https://travel.webcompose.id",
+  },
+];
+
+export const portoMobile = [
+  {
+    title: "Mentari Edukids",
+    desc: "Mentari Edukids is a tutoring institution that uses the Montessori teaching method as its curriculum. This project is a simple landing page that explains the programs and packages offered by this tutoring place and at the end of the page there will be a call to action that leads to the WhatsApp application for further communication.",
+    img: mentarim,
+    link: "https://mentariedukids.my.id",
+  },
+  {
+    title: "Digital Wedding Invitation",
+    desc: " A digitalized wedding invitation project to make it easier to send invitations and reduce paper. This project has parameters that can be filled in dynamically so that each invited guest has a private invitation.",
+    img: dayatm,
+    link: "https://www.dayatlubena.my.id/?to=Zawawi+dan+Eva",
+  },
+  {
+    title: "The Journey",
+    desc: "The Journey is a simple blog built using ReactJs and ExpressJS. This application has features for posting posts, commenting on posts and liking posts. This application is like a personal diary that we can share with other people.",
+    img: journeym,
+    link: "https://travel.webcompose.id",
   },
 ];
